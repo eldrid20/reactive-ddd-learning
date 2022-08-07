fe9934ada68f85ba5eddbaa559ed9a7f7410cd9a
eldrid.aditya <eldrid.aditya@cubepayment.com>
2022-08-07 14:41:35 +0200

    Removed Transactional.

--- src/main/java/com/xyz/payment/application/PaymentService.java
+++ src/main/java/com/xyz/payment/application/PaymentService.java
@@ -9,40 +9,38 @@ import com.xyz.payment.domain.model.PaymentItem;
 import com.xyz.payment.domain.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class PaymentService {
-  private final PaymentRepository paymentRepository;
+    private final PaymentRepository paymentRepository;
 
-  public Mono<Payment> createPayment(CreatePaymentCommand command) {
-    final var payment = Payment.create(PaymentItem.of(command.getAmount()));
-    return paymentRepository.savePayment(payment);
-  }
+    public Mono<Payment> createPayment(CreatePaymentCommand command) {
+        final var payment = Payment.create(PaymentItem.of(command.getAmount()));
+        return paymentRepository.savePayment(payment);
+    }
 
-  public Mono<Payment> addPaymentItem(AddPaymentItemCommand command) {
-    return findPayment(command.getPaymentId())
-        .map(payment -> payment.addItem(PaymentItem.of(command.getAmount())))
-        .flatMap(paymentRepository::savePayment);
-  }
+    public Mono<Payment> addPaymentItem(AddPaymentItemCommand command) {
+        return findPayment(command.getPaymentId())
+                .map(payment -> payment.addItem(PaymentItem.of(command.getAmount())))
+                .flatMap(paymentRepository::savePayment);
+    }
 
-  public Mono<Payment> completePayment(CompletePaymentCommand command) {
-    return findPayment(command.getPaymentId())
-        .map(Payment::complete)
-        .flatMap(paymentRepository::savePayment);
-  }
+    public Mono<Payment> completePayment(CompletePaymentCommand command) {
+        return findPayment(command.getPaymentId())
+                .map(Payment::complete)
+                .flatMap(paymentRepository::savePayment);
+    }
 
-  public Mono<Payment> getPayment(String paymentId) {
-    return findPayment(paymentId);
-  }
+    public Mono<Payment> getPayment(String paymentId) {
+        return findPayment(paymentId);
+    }
 
-  private Mono<Payment> findPayment(String paymentId) {
-    return paymentRepository
-        .findById(paymentId)
-        .switchIfEmpty(
-            Mono.error(new PaymentNotFoundException("Payment ID Not found :" + paymentId)));
-  }
+    private Mono<Payment> findPayment(String paymentId) {
+        return paymentRepository
+                .findById(paymentId)
+                .switchIfEmpty(
+                        Mono.error(new PaymentNotFoundException("Payment ID Not found :" + paymentId)));
+    }
 }
